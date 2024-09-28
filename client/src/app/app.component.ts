9ef73b1477647df8ca7e029b68c2901e711044d6
chisrra <chisrra-code@hotmail.com>
2024-09-28 17:26:29 -0600

    Parcial 01 - Create mkcert and SSL

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -16,7 +16,7 @@ export class AppComponent implements OnInit {
   users: any;
 
   ngOnInit(): void {
-    this.http.get("http://localhost:5189/api/v1/users/").subscribe({
+    this.http.get("https://localhost:5189/api/v1/users/").subscribe({
       next: (response) => { this.users = response; },
       error: (error) => { console.error(error); },
       complete: () => { console.log("Request Completed"); }
